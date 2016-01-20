fd9f51b3d6dba91b6502abb0a8bb60179533cbdc
Maria <maria.alcocer@fundacion-jala.org>
2016-01-19 20:51:37 -0400

    test commit

--- js/main.js
+++ js/main.js
@@ -39,13 +39,14 @@ var Estudiante = function(name, age) {
 var doOperations = function(number){
 
     console.log('MAX is', getMaxNumber(number, arguments.length));
-    //console.log('MIN is', getMinNumber());
-    //console.log('AVERAGE is', getAvgerage());
-    //console.log('SUM', getSum());
+    console.log('MIN is', getMinNumber());
+    console.log('AVERAGE is', getAvgerage());
+    console.log('SUM', getSum());
 };
 
 var getMaxNumber = function(numbers, size){
     if(numbers[size-1]>numbers[size-2])
         return numbers[size-1];
+
 };
 
