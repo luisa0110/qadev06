4820655db8019ed46ab3578e2426186b07f99ac6
Maria <maria.alcocer@fundacion-jala.org>
2016-01-25 20:36:13 -0400

    Start getSum in the first task

---
+++ js/task1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mariaalcocer on 1/25/2016.
+ */
+var doOperations = function(number){
+
+    console.log('MAX is', getMaxNumber(number, arguments.length));
+    console.log('MIN is', getMinNumber());
+    console.log('AVERAGE is', getAvgerage());
+    console.log('SUM', getSum());
+};
+
+var getMaxNumber = function(numbers, size){
+    if(numbers[size-1]>numbers[size-2])
+        return numbers[size-1];
+
+};
+
+
+
