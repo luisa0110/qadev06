f140f81128c33b5419775bae7bc100f607519f75
Maria <maria.alcocer@fundacion-jala.org>
2016-01-26 22:17:58 -0400

    getMax function in progress

--- js/task1.js
+++ js/task1.js
@@ -1,17 +1,40 @@
 /**
  * Created by mariaalcocer on 1/25/2016.
  */
-var doOperations = function(number){
-
-    console.log('MAX is', getMaxNumber(number, arguments.length));
-    console.log('MIN is', getMinNumber());
-    console.log('AVERAGE is', getAvgerage());
-    console.log('SUM', getSum());
+function Calculator(){
+    this.major = 0;
+}
+Calculator.prototype.doOperations = function(){
+    var number = arguments;
+    console.log('MAX is', this.getMaxNumber(number));
+    //console.log('MIN is', getMinNumber());
+    //console.log('AVERAGE is', getAvgerage());
+    //console.log('SUM', getSum());
 };
 
-var getMaxNumber = function(numbers, size){
-    if(numbers[size-1]>numbers[size-2])
-        return numbers[size-1];
+Calculator.prototype.getMaxNumber = function(numbers){
+    var collection =[];
+    collection = numbers;
+    Calculator.prototype.getMax = function(collection, size, max){
+        var major = majorNumber;
+        if(size >= 0) {
+            if(major > collection[cant-1])
+            {
+
+                return this.getMax(collection, cant - 1, major)
+            }
+            else
+            {
+                major = collection[cant-1];
+                return this.getMax(collection, cant - 1, collection[cant-1])
+            }
+        }
+        else
+        {
+            return major;
+        }
+    }
+    return var Max = this.getMax(collection, collection.length - 1, collection[collection.length - 1]);
 };
 
 
